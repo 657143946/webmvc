84fb32c695c0cba69f1b17f300d1d9c9203d352a
chrislee <657143946@qq.com>
2015-12-17 17:57:30 +0800

    first commit

---
+++ src/main/java/me/leemo/springmvc/Temp.java
@@ -0,0 +1,11 @@
+package me.leemo.springmvc;
+
+/**
+ * Created by Leemo on 15-12-17
+ * 第一个类，没有特殊要求，可以在这里测试一些片段代码
+ */
+public class Temp {
+    public static void main(String[] args){
+        System.out.println("start...");
+    }
+}
