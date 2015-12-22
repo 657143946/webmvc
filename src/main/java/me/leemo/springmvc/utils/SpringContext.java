0c5a5ad8bcb33e970323b2348a49829cda56b97d
Leemo <657143946@qq.com>
2015-12-22 11:34:46 +0800

    新增：获取spring组件类

---
+++ src/main/java/me/leemo/springmvc/utils/SpringContext.java
@@ -0,0 +1,22 @@
+package me.leemo.springmvc.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Created by ChrisLee on 15-5-24.
+ * spring上下文
+ */
+public class SpringContext implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    public static ApplicationContext getContext() {
+        return context;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContext.context = applicationContext;
+    }
+}
