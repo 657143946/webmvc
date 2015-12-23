f10398e880826754199dc4e914c67e264ba3459e
Leemo <657143946@qq.com>
2015-12-23 10:36:56 +0800

    新增：controller模板，包含了返回json和返回页面的使用方法

---
+++ src/main/java/me/leemo/springmvc/controller/HelloWorld.java
@@ -0,0 +1,47 @@
+package me.leemo.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Leemo on 15-12-22
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloWorld {
+    @RequestMapping("hello.page")
+    public ModelAndView page(
+            @RequestParam(defaultValue = "null") String value
+    ){
+        ModelAndView mv = new ModelAndView("hello.html");
+        mv.addObject("value", value);
+        return mv;
+    }
+
+    @RequestMapping("hello.json")
+    @ResponseBody
+    public Map<String, String> json(
+            @RequestParam(defaultValue = "null") String value
+    ){
+        Map<String, String> model = new HashMap<String, String>();
+        model.put("value", value);
+        return model;
+    }
+
+    @RequestMapping("error.page")
+    public ModelAndView errorPage(){
+        throw new RuntimeException();
+    }
+
+    @RequestMapping("error.json")
+    @ResponseBody
+    public ModelAndView errorJson(){
+        throw new RuntimeException();
+    }
+}
