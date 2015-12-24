101e70e964fbfda6175d8a597bdf65754dfeadfa
Leemo <657143946@qq.com>
2015-12-24 17:46:18 +0800

    新增：添加一个DAO support，主要是帮助注入配置好的数据库

---
+++ src/main/java/me/leemo/springmvc/dao/GraduateDAOSupport.java
@@ -0,0 +1,16 @@
+package me.leemo.springmvc.dao;
+
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by Leemo on 15-12-24
+ */
+public class GraduateDAOSupport extends HibernateDaoSupport {
+    @Resource(name = "graduate_sessionFactory")
+    public void setSuperSessionFactory(SessionFactory sessionFactory){
+        super.setSessionFactory(sessionFactory);
+    }
+}
